f70dd1791bccbc757bc00a4c6c1bbcaec4045865
Axel <axeljkern@yahoo.com>
2024-04-10 10:35:43 -0400

    Adds begin() and end() to Pstr.

--- src/models/pstr.h
+++ src/models/pstr.h
@@ -15,6 +15,9 @@ template <class t> struct pstr {
 	const char* Print() const;
 	const std::size_t Size() const;
 
+	const char* begin() const;
+	const char* end() const;
+
 	pstr& operator=(const pstr<t> &p);
 	friend std::ostream& operator<<(std::ostream &out, const pstr<t> &p) {
 		if (&out == &std::cout)
@@ -73,6 +76,12 @@ template <class t> const char* pstr<t>::Print() const {
 }
 template <class t> const std::size_t pstr<t>::Size() const {
 	return prefix_size_ + static_cast<size_t>(prefix_);
+}
+template <class t> const char* pstr<t>::begin() const {
+	return data_ + prefix_size_;
+}
+template <class t> const char* pstr<t>::end() const {
+	return data_ + prefix_size_ + prefix_;
 }
 
 template<class t> pstr<t>& pstr<t>::operator=(const pstr<t> &p) {
